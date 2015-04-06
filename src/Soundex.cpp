6b07652e7ba4edbfb2c9eb5f08c0cada7f3ce18f
Blubb <r.habrich@googlemail.com>
2015-04-06 10:53:27 +0200

    Added test case ReplacesConsonantsWithAppropriateDigits

--- src/Soundex.cpp
+++ src/Soundex.cpp
@@ -2,10 +2,15 @@
 
 std::string Soundex::encode(const std::string& word) const
 {
-    return zeroPad(word);
+    auto encoded = word.substr(0, 1);
+
+    if (word.length() > 1)
+        encoded += "1";
+    return zeroPad(encoded);
 }
 
 std::string Soundex::zeroPad(const std::string& word) const
 {
-    return word + "000";
+    auto zerosNeeded = 4 - word.length();
+    return word + std::string(zerosNeeded, '0');
 }
