c2762154514998bdced473a3b0a3be33ff2a13c6
Blubb <r.habrich@googlemail.com>
2015-04-06 10:57:39 +0200

    Refactoring

--- src/Soundex.cpp
+++ src/Soundex.cpp
@@ -2,15 +2,29 @@
 
 std::string Soundex::encode(const std::string& word) const
 {
-    auto encoded = word.substr(0, 1);
-
-    if (word.length() > 1)
-        encoded += "1";
-    return zeroPad(encoded);
+    return zeroPad(head(word) + encodedDigits(word));
 }
 
+std::string Soundex::head(const std::string& word) const
+{
+    return word.substr(0, 1);
+}
+
+std::string Soundex::encodedDigits(const std::string& word) const
+{
+    if (word.length() > 1)
+        return encodedDigit();
+    return "";
+}
+
+std::string Soundex::encodedDigit() const
+{
+    return "1";
+}
+
+static const size_t MaxCodeLength = 4;
 std::string Soundex::zeroPad(const std::string& word) const
 {
-    auto zerosNeeded = 4 - word.length();
+    auto zerosNeeded = MaxCodeLength - word.length();
     return word + std::string(zerosNeeded, '0');
 }
