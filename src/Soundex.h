df02fa8b7da0f5f62e7bc45ec03a01fc04b77b5a
Blubbz0r <r.habrich@googlemail.com>
2015-04-06 18:49:26 +0200

    Added minimal documentation to Soundex class (needs improvement though)

--- src/Soundex.h
+++ src/Soundex.h
@@ -3,6 +3,15 @@
 
 #include <string>
 
+/*!
+ * \brief Implements the Soundex algorithm using the following rules:
+ *          - b, f, p, v -> 1
+ *          - c, g, j, k, q, s, x, z -> 2
+ *          - d, t -> 3
+ *          - l -> 4
+ *          - m, n -> 5
+ *          - r -> 6
+ */
 class Soundex
 {
 public:
